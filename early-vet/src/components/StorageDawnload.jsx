d466b860bf73274f0aea46892ff3348a9d3b308b
Abdi Geremew <abdiggebeyehu@gmail.com>
2025-03-30 22:21:13 +0300

    Update StorageDawnload.jsx

--- early-vet/src/components/StorageDawnload.jsx
+++ early-vet/src/components/StorageDawnload.jsx
@@ -82,7 +82,7 @@ const NeedHelpPage = () => {
       setFormData({ fullName: "", email: "", contactNumber: "", message: "" });
       setCharCount(0);
       setErrors({});
-    }, 460); // 3000ms = 3 seconds
+    }, 500); // 3000ms = 3 seconds
 
     // Send request to backend immediately in the background
     try {
